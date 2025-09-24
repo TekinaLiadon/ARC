65b70484cbdb8e42620661dd834ee7ebef2f0d4d
grik <grik.nota2@gmail.com>
2025-09-24 23:42:37 +0300

    initial2

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    hei
+    <NuxtLayout />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+  dir: {
+    pages: "02-pages",
+    layouts: "01-app/layouts"
+  },
   vite: {
     server: {
       hmr: {
